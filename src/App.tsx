4a1f92b372d520d5d0f81a84a60e2428f41df4e0
Siddhant Khare <Siddhantkhare2694@gmail.com>
2023-06-23 05:12:32 +0000

    cleanup & Username based usage barchart

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Rechart from "./rechart";
+import FileInput from "./fileInput";
+import { parse } from "papaparse";
+import { ParseResult } from "papaparse";
 
 function App() {
+  const [csvData, setCsvData] = useState(null);
+
+  const getCsvFile = (file: string | File | NodeJS.ReadableStream) => {
+    let csvArr:
+      | {
+          userName: string;
+          teamId: string;
+          startTime: string;
+          contextUrl: string;
+          credits: number;
+        }[]
+      | React.SetStateAction<null>
+      | unknown[][] = [];
+
+    parse(file, {
+      worker: true,
+      step: (result: ParseResult<any>) => {
+        // @ts-ignore
+        csvArr.push(result.data);
+      },
+      complete: () => {
+        //remove last element of the CSV-Array (empty string)
+        // @ts-ignore
+        csvArr.pop();
+        //remove first element of the CSV-Array (table headers)
+        // @ts-ignore
+        csvArr.shift();
+
+        //transform CSV Arr into objects
+        // @ts-ignore
+        const csvDataStructuredAsObjects = csvArr.map((value) => {
+          return {
+            userName: value[10],
+            teamId: value[1].slice(5),
+            startTime: value[11].slice(0, 10),
+            contextUrl: value[12],
+            credits: parseFloat(value[3]),
+          };
+        });
+
+        //structure CSV-Data by date
+        const csvDataStructuredByStartTime = csvDataStructuredAsObjects.reduce(
+          // @ts-ignore
+          (acc, obj) => {
+            const key = obj.startTime;
+            if (!acc[key]) {
+              acc[key] = [];
+            }
+            acc[key].push(obj);
+            return acc;
+          },
+          {}
+        );
+        console.log(csvDataStructuredByStartTime);
+        // @ts-ignore
+        setCsvData(csvDataStructuredByStartTime);
+      },
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FileInput callback={getCsvFile} />
+      {csvData ? <Rechart csvData={csvData} /> : ""}
     </div>
   );
 }
