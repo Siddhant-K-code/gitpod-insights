9efba1bffaae29f223ca59234c8234cf86e36f55
Siddhant Khare <Siddhantkhare2694@gmail.com>
2023-06-23 10:43:31 +0530

    cleanup & Username based usage barchart (#1)

---
+++ src/fileInput.tsx
@@ -0,0 +1,26 @@
+import React, { useRef } from "react";
+
+// @ts-ignore
+const FileInput = ({ callback }) => {
+  const fileInput = useRef(null);
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // @ts-ignore
+    callback(fileInput.current.files[0]);
+  };
+
+  return (
+    <div className="file-uploader">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Upload file:
+          <input type="file" ref={fileInput} accept=".csv" />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default FileInput;
