965ddf8be5e11528da60d06ba7ca62aec435e0d2
Siddhant Khare <Siddhantkhare2694@gmail.com>
2023-06-24 04:13:14 +0000

    💪 Optimal Group Entries

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import BillingChart from "./components/billing-chart";
 import FileInput from "./components/file-input";
-import { getCsvFile } from "./csv-reader";
+import { getCsvFile, UsageReportEntry } from "./csv-reader";
 
-export interface DailyEntry {
+export interface UsageReportCsvEntry {
   userName: string;
   teamId: string;
   startTime: string;
@@ -14,16 +14,15 @@ export interface DailyEntry {
 }
 
 const App = (): JSX.Element => {
-  const [csvData, setCsvData] = useState(null);
-  const handleFileSubmit = (file: string | File | NodeJS.ReadableStream) => {
-    // @ts-ignore
+  const [csvData, setCsvData] = useState<UsageReportEntry[] | null>(null);
+  const handleFileSubmit = (file: File) => {
     getCsvFile(file).then((res) => setCsvData(res));
   };
 
   return (
     <div className="App">
       <FileInput onSubmit={handleFileSubmit} />
-      {csvData ? <BillingChart csvData={csvData} /> : ""}
+      {csvData && <BillingChart csvData={csvData} />}
     </div>
   );
 };
